4a5b242a65e0c809b8e912e11e63f5fe042dbc69
hiteshdevashrayee <hiteshd@webgility.com>
2022-07-12 23:32:52 +0530

    Commit

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+  selectedHero?:Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+  /* hero: Hero={
+    id:1,
+    name:'Windstrom'
+  }; */
   constructor() { }
 
   ngOnInit(): void {
